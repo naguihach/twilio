d55e780cb1fb6b6b4787c3ff1f9641fc4d42fcc3
Nagui Hachelaf <128603183+naguihach@users.noreply.github.com>
2023-05-01 17:32:18 +0200

    Create process-key.php

---
+++ process-key.php
@@ -0,0 +1,18 @@
+<?php
+  header('Content-type: text/xml');
+  $userPressed = (int) $_POST['Digits'];
+  switch ($userPressed) {
+    case 1:
+      // Redirection vers l'URL de commande
+      echo '<Response><Redirect>https://candyshop25.site</Redirect></Response>';
+      break;
+    case 2:
+      // Message pour contacter le service client
+      echo '<Response><Say language="fr-FR" voice="alice">Le service client est actuellement fermé. Merci de réessayer plus tard.</Say></Response>';
+      break;
+    default:
+      // Message d'erreur pour les autres choix
+      echo '<Response><Say language="fr-FR" voice="alice">Désolé, nous n\'avons pas compris votre choix. Veuillez réessayer plus tard.</Say></Response>';
+      break;
+  }
+?>
